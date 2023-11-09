54854faebb35ac8ad1a23bdabd3da8f37c6033ca
= <=>
2023-11-09 21:33:52 +0900

    프로그래머스 알고리즘 - K번째 수

---
+++ Kst.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> temp;
+
+    // command[i][0] 와 command[i][1] 사이의 값이 같거나 command[i][1]이 1클 경우 경계값 주의 필요
+    for(int i = 0; i < commands.size(); i++) {
+        temp = array;
+        sort(temp.begin() + commands[i][0] - 1, temp.begin() + commands[i][1]);
+        answer.push_back(temp[commands[i][0] + commands[i][2]-2]);
+    }
+
+    return answer;
+}
